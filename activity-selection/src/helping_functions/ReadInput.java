090806aeed3a8b85cf90a83fd80c512044861a37
MariamAziz0 <ma27523@gmail.com>
2023-12-12 11:38:36 +0200

    Read the input Read the input from the file given its absolute path.

---
+++ activity-selection/src/helping_functions/ReadInput.java
@@ -0,0 +1,57 @@
+package helping_functions;
+
+import java.io.*;
+import java.util.*;
+
+public class ReadInput {
+
+    public static int[][] getInput(String filePath) {
+        File inputFile = new File(filePath);
+
+        if (!inputFile.exists()) {
+            System.err.println("File not found: " + filePath);
+            return null;
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            int numActivities = Integer.parseInt(reader.readLine());
+
+            int[][] activities = new int[numActivities][3];
+
+            for (int i = 0; i < numActivities; i++) {
+                String line = reader.readLine();
+                if (line != null) {
+                    String[] pointTokens = line.split(" ");
+                    if (pointTokens.length == 3) {
+                        activities[i][0] = Integer.parseInt(pointTokens[0]);
+                        activities[i][1] = Integer.parseInt(pointTokens[1]);
+                        activities[i][2] = Integer.parseInt(pointTokens[2]);
+                    } else {
+                        System.err.println("Invalid format for point data on line " + (i + 2));
+                        return null;
+                    }
+                } else {
+                    System.err.println("File ended prematurely after " + i + " activities.");
+                    return null;
+                }
+            }
+            return activities;
+
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static void printArray(int[][] array) {
+
+        for(int i = 0 ; i < array.length ; i++) {
+            for(int j = 0 ; j < array[0].length ; j++)
+                System.out.print(array[i][j] + " ");
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+
+}
