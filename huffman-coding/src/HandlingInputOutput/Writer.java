45ec17dfd8ee93df01d9b6e56168c18d3910d82f
MariamAziz0 <ma27523@gmail.com>
2023-12-15 20:53:53 +0200

    updates

--- huffman-coding/src/HandlingInputOutput/Writer.java
+++ huffman-coding/src/HandlingInputOutput/Writer.java
@@ -2,6 +2,9 @@ package HandlingInputOutput;
 
 import java.io.*;
 
+/**
+ * I acknowledge that I am aware of the academic integrity guidelines of this course, and that I worked on this assignment independently without any unauthorized help
+ */
 public class Writer {
 
     private FileOutputStream fileOutputStream;
