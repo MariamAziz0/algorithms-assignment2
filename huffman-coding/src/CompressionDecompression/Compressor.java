adde936af151374aa3933e3ca7f9b09c549ca5b2
MariamAziz0 <ma27523@gmail.com>
2023-12-19 15:56:45 +0200

    Compressor is implemented

---
+++ huffman-coding/src/CompressionDecompression/Compressor.java
@@ -0,0 +1,105 @@
+package CompressionDecompression;
+import java.util.*;
+
+import Convertion.ByteToBinary;
+import Convertion.ByteToHex;
+import HandlingInputOutput.*;
+import HuffmanCoding.HuffmanCodingAlgorithm;
+
+public class Compressor {
+    private int bufferSize;
+    private Reader reader;
+    private Writer writer;
+    private String inputPath;
+    private String outputPath;
+    private int n;
+    private HuffmanCodingAlgorithm huffman;
+    // TODO: to be removed
+    private Map<String, String> codeWords;
+    private long outputBytes;
+    private byte bitsInLastByte;
+
+    public Compressor (String inputPath, int n, int bufferSize) {
+        this.inputPath = inputPath;
+        this.n = n;
+        this.bufferSize = bufferSize;
+
+        this.reader = new Reader(this.inputPath, this.bufferSize);
+        this.outputPath = HelpingFunctions.getCompressedPath(this.inputPath, this.n);
+        this.writer = new Writer(this.outputPath);
+        this.huffman = new HuffmanCodingAlgorithm();
+
+        // TODO: to be removed
+        this.codeWords = new HashMap<>();
+        this.outputBytes = 0;
+        this.bitsInLastByte = 0;
+    }
+
+    public void compress () {
+
+        byte[] bytesRead;
+        while ((bytesRead = this.reader.readChunk()) != null) {
+            String[] hexRepresentation = ByteToHex.byteArrayToHexStringArray(bytesRead, this.n);
+            this.huffman.updateFrequency(hexRepresentation);
+        }
+
+//        Map<String, String> codeWords = this.huffman.applyHuffman();
+        this.codeWords = this.huffman.applyHuffman();
+
+        // Open new Reader to read file from beginning and apply huffman
+        this.reader = new Reader(this.inputPath, this.n);
+        String bitCompressed = "";
+
+        while ((bytesRead = this.reader.readChunk()) != null) {
+            String[] hexRepresentation = ByteToHex.byteArrayToHexStringArray(bytesRead, this.n);
+            for (String sequence : hexRepresentation) {
+                bitCompressed += codeWords.get(sequence);
+            }
+
+            if (bitCompressed.length() > 8) {
+                String readyToWrite = bitCompressed.substring(0, (bitCompressed.length() / 8) * 8);
+                bitCompressed = bitCompressed.substring((bitCompressed.length() / 8) * 8);
+
+                byte[] bytesToWrite = ByteToBinary.bitStringToByteArray(readyToWrite);
+                this.writer.writeChunk(bytesToWrite);
+            }
+        }
+
+        // If there is remaining bits to written in separate byte
+        if (!bitCompressed.equals("")) {
+            byte lastByte = ByteToBinary.binaryStringToByteWithTrailingZeros(bitCompressed);
+            byte[] lastByteArray = {lastByte};
+            this.writer.writeChunk(lastByteArray);
+        }
+
+        this.writer.close();
+
+        //TODO: to be removed
+        this.outputBytes = this.huffman.getNumberBytesOutput();
+        this.bitsInLastByte = this.huffman.getNumberBitsInLastByte();
+
+    }
+
+    // TODO: to be modified
+    public Map<String, String> getReversedCodeWords () {
+
+        Map<String, String> reversedCodeWords = new HashMap<>();
+        for (Map.Entry<String, String> entry : this.codeWords.entrySet()) {
+            reversedCodeWords.put(entry.getValue(), entry.getKey());
+        }
+
+        return reversedCodeWords;
+
+    }
+
+    // TODO: to be removed
+
+
+    public long getOutputBytes() {
+        return outputBytes;
+    }
+
+    public byte getBitsInLastByte() {
+        return bitsInLastByte;
+    }
+}
--- huffman-coding/src/HandlingInputOutput/Writer.java
+++ huffman-coding/src/HandlingInputOutput/Writer.java
@@ -29,18 +29,21 @@ public class Writer {
             return true;
         }
         catch (Exception e) {
-
-            try {
-                this.bufferedOutputStream.close();
-                this.fileOutputStream.close();
-            }
-            catch (Exception e2) {
-                System.out.println("Error in closing the stream of Output: " + e2.getMessage());
-            }
-
             System.out.println("Error in writing chunk of data: " + e.getMessage());
+            return close();
         }
-        return false;
+    }
+
+    public boolean close () {
+        try {
+            this.bufferedOutputStream.close();
+            this.fileOutputStream.close();
+            return true;
+        }
+        catch (Exception e) {
+            System.out.println("Error in closing the stream of Output: " + e.getMessage());
+            return false;
+        }
     }
 
 }
